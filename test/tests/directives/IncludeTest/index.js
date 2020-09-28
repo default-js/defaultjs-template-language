649d381fe842b8310330c4ebec8eafee38755d4e
Frank Schüler <schuelerfrank@gmx.de>
2020-09-28 13:57:56 +0200

    new include test case

--- test/tests/directives/IncludeTest/index.js
+++ test/tests/directives/IncludeTest/index.js
@@ -12,6 +12,26 @@ describe("Include Test - ", () => {
 		expect(result.children.length).toBe(1);
 		result = result.children[0];
 		expect(result.children.length).toBe(5);
+		container.remove();
+	});
+
+	it("load template from url after if", async () => {		
+		const container = create("<div></div>").first();		
+		const renderer = await Renderer.build({template: `<div jstl-if="\${include}" jstl-include="/templates/IncludeTest/LoadFromURL.tpl.html"></div>`});
+		await renderer.render({container, data : {include: true}});		
+		let result = container;
+		expect(result.children.length).toBe(1);
+		result = result.children[0];
+		expect(result.children.length).toBe(5);
+		container.remove();
+	});
+
+	it("load template from url after if on jstl-tag", async () => {		
+		const container = create("<div></div>").first();		
+		const renderer = await Renderer.build({template: `<jstl jstl-if="\${include}" jstl-include="/templates/IncludeTest/LoadFromURL.tpl.html"></jstl>`});
+		await renderer.render({container, data : {include: true}});		
+		expect(container.children.length).toBe(5);
+		container.remove();
 	});
 	
 	afterAll(() => {});
