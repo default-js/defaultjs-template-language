f1948bb0ae0762d1e54f615533831640b8de948d
Frank Schüler <schuelerfrank@gmx.de>
2020-09-29 20:06:29 +0200

    better logic of initial directive

--- src/directives/Initial.js
+++ src/directives/Initial.js
@@ -7,7 +7,7 @@ class Initial extends Directive {
 		super();
 	}
 
-	get name() { return "initial"  }
+	get name() { return "initial" }
 	get rank() { return Directive.MIN_RANK }
 	get phase() { return Directive.PHASE.init }
 
@@ -15,8 +15,21 @@ class Initial extends Directive {
 	async execute(context) {
 		const { template, renderer } = context;
 		if (template instanceof Text)
-			context.content = document.importNode(template, true);		
-		else if(template instanceof HTMLTemplateElement){
+			context.content = document.importNode(template, true);
+
+		else if (template.attr("jstl-async")) {
+			context.content = new Replace();
+			template.attr("jstl-async", null);
+			setTimeout(async () => {
+				await renderer.render({ mode: "replace", target: context.content, context });
+			}, parseInt(template.attr("jstl-async") || "250") || 250);
+			context.stop = true;
+			context.ignore = true;
+		} else if (template.attr("jstl-ignore")) {
+			context.content = document.importNode(template, true);
+			context.stop = true;
+			context.ignore = true;
+		} else if (template instanceof HTMLTemplateElement) {
 			context.content = document.createElement(template.tagName);
 			const subcontext = context.subContext({
 				template: template.content.childNodes,
@@ -25,23 +38,9 @@ class Initial extends Directive {
 			renderer.render({ context: subcontext });
 			context.stop = true;
 			context.ignore = true;
-		} else if (template instanceof HTMLElement) {
-			if (template.attr("jstl-async")) {
-				context.content = new Replace();
-				template.attr("jstl-async", null);
-				setTimeout(async () => {
-					await renderer.render({ mode: "replace", target: context.content, context });
-				}, parseInt(template.attr("jstl-async") || "250") || 250);
-				context.stop = true;
-				context.ignore = true;
-			} else if (template.attr("jstl-ignore")) {
-				context.content = document.importNode(template, true);
-				context.stop = true;
-				context.ignore = true;
-			} else if (template.tagName) {
-				context.content = document.createElement(template.tagName);
-			}
-		} else {
+		} else if (template.tagName)
+			context.content = document.createElement(template.tagName);
+		else {
 			context.content = document.importNode(template, true);
 			context.stop = true;
 			context.ignore = true;
